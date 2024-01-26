766e957b88964783b01c94f2df3db5a079e6ca38
hexiaoyu <hexi.aoyu@163.com>
2024-01-26 11:03:29 +0800

    Simple SQL Compiler

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # dbonc
 A SQLite like database for first learner, build from scratch.
+
+
 [reference](https://cstack.github.io/db_tutorial/)
+
+
 Basically I just translated it into Chinese, probably with a little bit of my own understanding.
--- dbonc.c
+++ dbonc.c
@@ -51,8 +51,59 @@ void read_input(InputBuffer* input_buffer) {
 }
 
 void close_input_buffer(InputBuffer* input_buffer) {
-    free(input_buffer->buffer);
-    free(input_buffer);
+  free(input_buffer->buffer);
+  free(input_buffer);
+}
+
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+  PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum { 
+  STATEMENT_INSERT, STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+  StatementType type;
+} Statement;
+
+/*如果执行的是metacommand，就通过这个方法来处理*/
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer,
+                                Statement* statement) {
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  if (strcmp(input_buffer->buffer, "select") == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+  switch (statement->type) {
+    case (STATEMENT_INSERT):
+      printf("这里以insert开头，应该执行一个insert语句.\n");
+      break;
+    case (STATEMENT_SELECT):
+      printf("这里以select开头，应该执行一个insert语句.\n");
+      break;
+  }
 }
 
 int main(int argc, char const *argv[])
@@ -63,12 +114,27 @@ int main(int argc, char const *argv[])
     print_prompt();
     read_input(input_buffer);
 
-    // 如果以.exit结尾就关闭命令行
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("无效语句： '%s'.\n", input_buffer->buffer);
+    // 把以点.开始的命令叫做 MetaCommand，直译为元命令语言，这里指除了SQL之外的语言
+    if (input_buffer->buffer[0] == '.') {
+      switch (do_meta_command(input_buffer)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("无效语句： '%s'\n", input_buffer->buffer);
+          continue;
+      }
     }
+ 
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("无效语句 '%s'.\n",
+               input_buffer->buffer);
+        continue;
+    }
+ 
+    execute_statement(&statement);
   }
 }
